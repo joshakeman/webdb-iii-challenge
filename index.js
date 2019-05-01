d78933f13ad40ea4c73f0c8147dc6c4bf5a46ac4
Josh Akeman <joshakeman@gmail.com>
2019-05-01 16:07:05 -0400

    server running and cohorts migration working

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const helmet = require('helmet')
+const knex = require('knex')
+
+const knexConfig = {
+    client: 'sqlite3',
+    connection: {
+        filename: './dev.sqlite3'
+    },
+    useNullAsDefault: true
+}
+
+const db = knex(knexConfig)
+
+const server = express()
+
+server.use(helmet())
+server.use(express.json)
+
+server.get('api/cohorts', async (req, res) => {
+    try {
+        const cohorts = await db('cohorts')
+        res.status(200).json(cohorts)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+server.listen(5000, () => 
+    console.log(`\n** API running on 5000 **\n`)
+)
